f6f47729e484c8e7ffcc719902d82ee81f651a70
Kate Raskauskas <katherine.raskauskas@gmail.com>
2019-12-03 03:34:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,7 +33,12 @@ def monopoly_with_third_tier
         key3: 3,
         key4: 4
       },
-      :names => "Yukon"
+      :names => {
+        key1: 1,
+        key2: 2,
+        key3: 3,
+        key4: 4
+      }
     }
   }
   
