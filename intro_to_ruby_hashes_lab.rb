58dbbeb05e1a26c2c9983be8683dd582dd17f94e
Kate Raskauskas <katherine.raskauskas@gmail.com>
2019-12-03 03:37:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,7 +37,7 @@ def monopoly_with_third_tier
         reading_railroad: {},
         b_and_o_railroad: {},
         shortline_railroad: {},
-        key4: {}
+        pennsylvania_railroad: {}
       }
     }
   }
