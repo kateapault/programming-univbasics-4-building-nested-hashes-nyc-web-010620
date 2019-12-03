5b26f7e6bbf49fc2703199e7efd5c23a6840a47a
Kate Raskauskas <katherine.raskauskas@gmail.com>
2019-12-03 03:32:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,9 +11,11 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   h = {
-    railroads: {}
+    railroads: {
+      :pieces => "pie"
+    }
   }
-  h[:pieces] = "pie"
+  
   h
 end
 
