eb9f4ea5fb3883f8c71f82917b79f16d8e6d4670
Kate Raskauskas <katherine.raskauskas@gmail.com>
2019-12-03 03:33:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,12 @@ def monopoly_with_third_tier
   h = {
     railroads: {
       :pieces => 4,
-      :rent_in_dollars => "8"
+      :rent_in_dollars => {
+        key1: 1,
+        key2: 2,
+        key3: 3,
+        key4: 4
+      }
     }
   }
   
