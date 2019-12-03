c2424b315ebbc79a0f0cf54d2a93c3616eb927f6
Kate Raskauskas <katherine.raskauskas@gmail.com>
2019-12-03 03:40:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -62,7 +62,7 @@ def monopoly_with_fourth_tier
       },
       :names => {
         reading_railroad: {
-          mortgage_value: "$100"
+          "mortgage_value" => "$100"
         },
         b_and_o_railroad: {
           mortgage_value: "$400"
